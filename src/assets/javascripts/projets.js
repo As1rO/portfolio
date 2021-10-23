870dd3e6d29a4fad6ecc6cea180c1a0f52dc7ae4
olivier <olivier.denis@hotmail.be>
2021-10-23 17:13:04 +0200

    css opti + js opti

---
+++ src/assets/javascripts/projets.js
@@ -0,0 +1,27 @@
+var data = require("../javascripts/json/projets.json");
+
+const dataString = document.querySelectorAll("[data-key]");
+
+var contentEl = document.querySelector(".content-2").children;
+
+var ct3 = document.querySelector(".content-2");
+var buttonInfo = document.querySelector(".button-info");
+var info = document.querySelector(".info");
+var imgOpen = document.querySelector(".img-open");
+
+for (let i = 0; i < 4; i++) {
+  contentEl[i].addEventListener("click", (e) => {
+    ct3.classList.add("blur");
+    info.classList.remove("display");
+    dataString.forEach((element) => {
+      let value = element.getAttribute("data-key");
+      element.innerHTML = data[i][value];
+      imgOpen.setAttribute("src", data[i].image);
+    });
+  });
+}
+
+buttonInfo.addEventListener("click", (e) => {
+  ct3.classList.remove("blur");
+  info.classList.add("display");
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
 import "./assets/javascripts/app";
+import "./assets/javascripts/animation";
+import "./assets/javascripts/nav";
+import "./assets/javascripts/projets";
